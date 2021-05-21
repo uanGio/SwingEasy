25b375be9330fd280ac987723e8575ca9c59f1b3
Jp3222 <juanpc.559gmail.com>
2021-05-21 12:09:58 -0500

    se agrego un boton

--- src/com/uangio/swingeasy/Main.java
+++ src/com/uangio/swingeasy/Main.java
@@ -1,18 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.uangio.swingeasy;
 
-/**
- *
- * @author giovanny
- */
+import javax.swing.JButton;
+
 public class Main {
-    
+
     public static void main(String[] args) {
         System.out.println("Hola mundo");
     }
-    
+
+    public JButton drawButton(String text, int x, int y, int sizeX, int sizeY) {
+        JButton obj = new JButton();
+        obj.setText(text);
+        obj.setSize(sizeX, sizeY);
+        obj.setLocation(x, y);
+        return obj;
+    }
+
 }
