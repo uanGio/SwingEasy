2eb5c325df5afbeaa61471c49253dcb8092cd260
Jp3222 <juanpc.559gmail.com>
2021-05-21 20:42:49 -0500

    Modificacion en las canvas y en la Table

---
+++ src/com/uangio/swingeasy/Table/Table.java
@@ -0,0 +1,36 @@
+package com.uangio.swingeasy.Table;
+
+import com.uangio.swingeasy.Canva.Canva;
+import java.awt.Dimension;
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+public class Table extends JFrame {
+
+    public Table(String title, int SizeX, int SizeY) {
+        Table(title, SizeX, SizeY, 0, 0, true);
+    }
+
+    public Table(String title, int SizeX, int SizeY, int PX, int PY) {
+        Table(title, SizeX, SizeY, PX, PY, false);
+    }
+
+    private void Table(String title, int SizeX, int SizeY, int PX, int PY, boolean center) {
+        this.setTitle(title);
+        this.setSize(SizeX, SizeY);
+        this.setLocation(PX, PY);
+        if (center) {
+            setLocationRelativeTo(this);
+        }
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public void setCanva(Canva c) {
+        this.setContentPane(c);
+    }
+
+    public void Painting() {
+        setVisible(true);
+    }
+
+}
