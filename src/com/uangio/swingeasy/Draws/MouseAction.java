8e2cf0efa26bb5ba805ff0f5fcbb5b2498d69636
giovanny-b <bernal.giovanny1402@gmail.com>
2021-05-21 20:33:05 -0500

    se agrego botones con MouseListener, label

---
+++ src/com/uangio/swingeasy/Draws/MouseAction.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.uangio.swingeasy.Draws;
+
+/**
+ *
+ * @author giovanny
+ */
+public interface MouseAction {
+    
+    public void leftClick();
+    public void centerClick();
+    public void rightclick();
+    
+    public void insideMouse();
+    public void outsideMouse();
+}
